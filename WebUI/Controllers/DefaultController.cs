7bf14bb6c02a376301a5221bbf82dc105d573984
lee cheng <nixiaozi01@hotmail.com>
2017-03-10 15:02:40 +0800

    1、建立了一个临时的DefaultController 和视图，对EF的使用。

---
+++ WebUI/Controllers/DefaultController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataBaseModel.Abstract;
+using DataBaseModel.AllEntity.UserInfo;
+using DataBaseModel.Concrete;
+
+namespace WebUI.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            EcomerceDBContext db = new EcomerceDBContext();
+            var the= db.UserSummarys.ToList();
+
+            
+
+            return View(the);
+        }
+    }
+}
