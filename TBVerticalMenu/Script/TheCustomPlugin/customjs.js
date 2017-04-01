5ce91032c5d789a057a57e19f1c1a9f46d8b29d1
lee cheng <nixiaozi01@hotmail.com>
2017-04-01 18:19:56 +0800

    1、添加了css对控件进行样式化 2、添加了li的mouseon事件绑定和控件级别的mouseout事件指定， 3、由于对event。target进行了判断所以必须保证需要的target在屏幕视图中占据部分顶层区域。 4、修改了模板以及json序列化

--- TBVerticalMenu/Script/TheCustomPlugin/customjs.js
+++ TBVerticalMenu/Script/TheCustomPlugin/customjs.js
@@ -7,6 +7,8 @@
             template: null,
             data: null,
             templateUrl: "plugin_tmp.html",
+            detailtemplate: null,
+            detailtempUrl: "plugin_detail_tmp.html",
 
 
         },
@@ -41,6 +43,63 @@
             var template = Handlebars.compile($target.options.template);
             var thehtml = template($target.options.data);
             $(thehtml).appendTo($target.element);
+
+            //移出元素区域取消选择 并且隐藏详情框
+            $target._on($target.element.find(".container"), {
+                mouseout: function (event) {
+                    if($(event.target).is("div.container")){
+                        $('.mainmenu-li.active').removeClass("active");
+                        $target.element.find(".mainmenu-detail").hide();
+                    }
+                }
+            });
+
+            //mouseover菜单条目事件 修改详情框内容并显示
+            $target._on($target.element.find(".mainmenu-li"), {
+                mouseover: function (event) {
+                    var $eventarget = $(event.target);
+                    if ($eventarget.is("li")) {
+                        $('.mainmenu-li.active').removeClass("active");
+                        $eventarget.addClass("active");
+                        var getid = parseInt($eventarget.attr("data-id"));
+                        this._getdetailtempAndDo(getid);
+                        $target.element.find(".mainmenu-detail").show();
+                    }
+
+                }
+            });
+
+        },
+        //根据Data中的数据初始化显示数据
+        _detailinit:function(itemid){
+            this.element.find(".mainmenu-detail").html("");
+            var itemdata = this.options.data.Mainmenu[itemid];
+            
+            var template = Handlebars.compile(this.options.detailtemplate);
+            var thehtml = template(itemdata);
+
+            this.element.find(".mainmenu-detail").html(thehtml);
+        },
+        //获得详细信息并且执行
+        _getdetailtempAndDo(itemid) {
+            var $target = this;
+            if (this.options.detailtemplate != null) {
+                $target._detailinit(itemid);//必须要指定this 否则无法找到该函数
+            } else {
+                $.ajax({
+                    type: "GET",
+                    contentType: "json", //这是上传时的数据类型
+                    url: $target.options.detailtempUrl,
+                    dataType: "html", //这是得到的响应数据类型
+                    success: function (data) {
+                        $target.options.detailtemplate = data;
+                        $target._detailinit(itemid);
+                    },
+                    error: function () {
+                        alert("服务端错误！");
+                    }
+                });
+            }
         },
         _refreash: function () {
             //_refreash 刷新函数初始化
